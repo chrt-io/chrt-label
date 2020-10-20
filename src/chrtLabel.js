e74293c8c0f72284ca1f072f18de64706881c682
carlo <littleark@gmail.com>
2020-10-20 09:16:54 +0000

    fixed automatic label position on last element of the linechart, added also margins({}) to manage the space around the label

--- src/chrtLabel.js
+++ src/chrtLabel.js
@@ -7,6 +7,7 @@ import {
   left,
   align,
   valign,
+  margins,
 } from './lib';
 import { createSVG as create } from '~/layout';
 
@@ -14,7 +15,7 @@ const DEFAULT_FILL_COLOR = '#000';
 
 function chrtLabel(text) {
   chrtGeneric.call(this);
-  // console.log('HI WE ARE MARKERS', this);
+  // console.log('chrtLabel', this);
   this.type = 'label';
   this.g = null;
   this.text = text;
@@ -23,6 +24,12 @@ function chrtLabel(text) {
   this._alignment = {
     horizontal: 'start',
     vertical: '0.25em',
+  }
+  this._margins = {
+    top: 0,
+    right: 0,
+    bottom: 0,
+    left: 0,
   }
 
   this.draw = () => {
@@ -34,15 +41,37 @@ function chrtLabel(text) {
       this.g = create('g');
       this.parentNode.g.appendChild(this.g);
     }
+    
 
-    const { scales } = this.parentNode.parentNode;
+    const { scales, _css } = this.parentNode.parentNode;
 
-    // console.log('chrtLabel', this._position)
+    // console.log('chrtLabel parent', this.parentNode.parentNode)
+
+    if(!isNull(_css)) {
+      this.g.classList.remove('chrt-label');
+      this.g.classList.add('chrt-label');
+    }
+
+    const getPosition = position => (field) => {
+      // console.log('getPosition', position, field)
+      if(isNull(position[field])) {
+        if(!isNull(this.parentNode._data) && this.parentNode._data.length) {
+          console.log('--->', this.parentNode._data)
+          return this.parentNode._data[this.parentNode._data.length - 1][field];
+        } else {
+          return 0;
+        }
+      }
+      return position[field];
+    };
 
     if(scales && scales['x']) {
-      const x = isNull(this._position.x) ? 0 : scales['x'](this._position.x);
+      const x = scales['x'](getPosition(this._position)('x')) + this._margins.left - this._margins.right;
       // if y is not defined by the user, it should be calculated based on the closest Y value based on X
-      const y = isNull(this._position.y) ? 0 : scales['y'](this._position.y);
+      const y = scales['y'](getPosition(this._position)('y')) + this._margins.top - this._margins.bottom;
+
+
+
       this.g.setAttribute('transform',`translate(${x},${y})`)
     }
 
@@ -73,6 +102,7 @@ chrtLabel.prototype = Object.assign(chrtLabel.prototype, {
   left,
   align,
   valign,
+  margins,
 });
 
 export default chrtLabel;
